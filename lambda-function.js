1d5997c356f52b51f528940a611ae401fca1ce7c
Matt Solnit <msolnit@soasta.com>
2016-03-07 21:44:50 -0800

    Support for running outside of Lambda.

--- lambda-function.js
+++ lambda-function.js
@@ -6,13 +6,15 @@ var aws = require('aws-sdk');
 var https = require('https');
 var url = require('url');
 
-exports.handler = function(event, context) {
-    console.log('Received event:', JSON.stringify(event, null, 2));
+var checkAllRegions = function(event, context) {
+    if (event) {
+        console.log('Received event:', JSON.stringify(event, null, 2));
+    }
 
     var ec2 = new aws.EC2();
     ec2.describeRegions({}, function(err, data) {
         if (err) {
-            context.fail(err);
+            handleFailure(err, context);
         } else {
             data.Regions.forEach(function(region) {
                 checkRegion(region.RegionName, context);
@@ -30,7 +32,7 @@ var checkRegion = function(regionName, context) {
 
     ec2.describeInstances({}, function(err, data) {
         if (err) {
-            context.fail(err);
+            handleFailure(err, context);
         } else {
             data.Reservations.forEach(function(reservation) {
                 reservation.Instances.forEach(function(instance) {
@@ -43,4 +45,23 @@ var checkRegion = function(regionName, context) {
             });
         }
     });
+}
+
+var handleFailure = function(err, context) {
+    if (context) {
+        // We have a Lambda context; invoke the official "failure" method.
+        context.fail(err);
+    } else {
+        // We don't have a Lambda context; assume we're running in standalone mode.
+        console.error(err);
+        process.exit(1);
+    }
+}
+
+// Expose the Lambda endpoint.
+exports.handler = checkAllRegions;
+
+// If we're running in standalone mode (i.e. outside of Lambda), then execute now.
+if (process && process.argv && process.argv.length === 3 && process.argv[2] === "--standalone") {
+    checkAllRegions();
 }
